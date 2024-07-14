f0d338ae83b2ea23f2cbb67d6f2f635d7c4539b0
Dornfeld Zoltán <dornfeldz@gmail.com>
2024-07-14 16:14:54 +0200

    fixing markers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,5 @@
 import { useEffect, useState } from "react";
 import Map from "./Map";
-import L from "leaflet";
-
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-});
 
 function App() {
   const [data, setData] = useState([]);
