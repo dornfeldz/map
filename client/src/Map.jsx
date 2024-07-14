9369effff21b95b9bd7db3f6d6ad82b8218fcdcc
Dornfeld Zoltán <dornfeldz@gmail.com>
2024-07-14 19:24:47 +0200

    onClick event

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -6,25 +6,12 @@ import {
   useMapEvents,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
-import L from "leaflet";
-
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
-});
+import "./leafletIconSetup";
 
 const MapClickHandler = () => {
   useMapEvents({
-    click: () => {
-      console.log("Hello World");
+    click: (e) => {
+      console.log(e.latlng);
     },
   });
   return null;
