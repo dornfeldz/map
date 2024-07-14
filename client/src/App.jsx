1e5d4445f3f6e4073a1cd7051d958fd6c0209d3f
Dornfeld Zoltán <dornfeldz@gmail.com>
2024-07-14 15:52:38 +0200

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/")
+    fetch("https://map-backend-tau.vercel.app/shops")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
