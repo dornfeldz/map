aaf14453235888a77fbc6d732f301b812df3732a
Dornfeld Zoltán <dornfeldz@gmail.com>
2024-07-21 21:21:08 +0200

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,26 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://map-backend-tau.vercel.app/shops")
+    // fetch("https://map-backend-tau.vercel.app/shops")
+    fetch("http://localhost:8000/shops")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
 
-  return <Map shops={data} />;
+  return (
+    // <div className="flex">
+    //   <div className="w-[50%] h-full">
+    <Map shops={data} />
+    //   </div>
+    //   <div className="p-10 flex flex-col">
+    //     <button className="">Button</button>
+    //     <button className="">Button</button>
+    //     <button className="">Button</button>
+    //     <button className="">Button</button>
+    //     <button className="">Button</button>
+    //   </div>
+    // </div>
+  );
 }
 
 export default App;
--- server/dbConn.js
+++ server/dbConn.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const URI = process.env.DB_URI;
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    console.log(typeof URI);
+    await mongoose.connect(URI);
   } catch (error) {
     console.log(error);
   }
