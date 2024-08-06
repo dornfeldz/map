6d9dcee23e48bc918e14f576c660d2f6dab767cc
Dornfeld Zoltán <dornfeldz@gmail.com>
2024-08-06 19:39:24 +0200

    Added loading indicator

--- client/src/ui/AppLayout.jsx
+++ client/src/ui/AppLayout.jsx
@@ -1,10 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import Navigation from "./Navigation";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
       <Header />
       <div className="overflow-x-scroll scrollbar-hide">
         <Outlet />
